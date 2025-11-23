bdd9dafdc3853a70329943103ef6394d8fa01bd2
deiker12345 <Deikergomez999@gmail.com>
2025-11-23 17:58:03 -0500

    feat(environments): add firebase and supabase configs to environment files
    Add configuration for Firebase and Supabase services to both production and development environment files to enable integration with these backend services.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,20 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDHr3rs78FeqmLc7X-hHyxVsEbPh3LpbpU",
+    authDomain: "ar-taller.firebaseapp.com",
+    projectId: "ar-taller",
+    storageBucket: "ar-taller.appspot.com",
+    messagingSenderId: "1086780579906",
+    appId: "1:1086780579906:web:f66595ef549f0227804ddb",
+    measurementId: "G-9ERQV2QDML"
+  },
+  supabase: {
+    url: "https://lhjqzrfkocsxaxgcsiqx.supabase.co",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxoanF6cmZrb2NzeGF4Z2NzaXF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5MDQ2MjgsImV4cCI6MjA3OTQ4MDYyOH0.wCu0rzQH1fx0tXW3AtZbUjckOhYChVy4VTko7V8jp34"
+  }
 };
 
 /*
